719880c0b2e830bac69d41106e8152eb4a763e00
Marina Belan <belanm77@gmail.com>
2024-01-31 22:25:17 +0600

    add file

---
+++ utils/config.js
@@ -0,0 +1,10 @@
+export const SECRET_CODE = 'some-secret-key';
+
+export const ALLOWED_CORS = [
+
+  'http://localhost:3000',
+  'http://localhost:3001',
+];
+
+export const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+export const MONGO_URL = 'mongodb+srv://admin:K9o4HPGZ9tAeGZIN@cluster0.r3zlyri.mongodb.net/shop?retryWrites=true&w=majority';
