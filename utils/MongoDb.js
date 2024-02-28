ecfb82c60caa82f9e47d2272e1c1f80ce425bcb8
Marina Belan <belanm77@gmail.com>
2024-02-28 21:30:02 +0600

    add env

--- utils/MongoDb.js
+++ utils/MongoDb.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 // eslint-disable-next-line import/extensions
-import { MONGO_URL } from './config.js';
+// import { MONGO_URL } from './config.js';
 
 const connectDataBase = async () => {
   try {
     // eslint-disable-next-line no-unused-vars
-    const connection = await mongoose.connect(MONGO_URL, {
+    const connection = await mongoose.connect(process.env.MONGO_URL, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,8 @@
 export const SECRET_CODE = 'some-secret-key';
 
 export const ALLOWED_CORS = [
-  'https://furniking-shop-bae5d1c00634.herokuapp.com/',
   'http://localhost:3000',
   'http://localhost:3001',
 ];
 
 export const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-export const MONGO_URL = 'mongodb+srv://admin:K9o4HPGZ9tAeGZIN@cluster0.r3zlyri.mongodb.net/shop?retryWrites=true&w=majority';
