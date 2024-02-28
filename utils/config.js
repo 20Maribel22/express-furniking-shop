978ce3aa2c9f196e4e8aafbfc2011f0b1f5d2ee6
Marina Belan <belanm77@gmail.com>
2024-02-28 21:51:25 +0600

    add url frontend

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,7 @@
 export const SECRET_CODE = 'some-secret-key';
 
 export const ALLOWED_CORS = [
+  'https://furniking-shop-frontend.onrender.com',
   'http://localhost:3000',
   'http://localhost:3001',
 ];
